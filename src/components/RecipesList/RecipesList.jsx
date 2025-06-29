5ee7ae98a045ddaba7b7aa3c3602bb7d0ecdc8b3
Dmytro Molodyk <129516260+Tap0kk@users.noreply.github.com>
2025-06-29 12:50:06 +0300

    Update RecipesList.jsx

--- src/components/RecipesList/RecipesList.jsx
+++ src/components/RecipesList/RecipesList.jsx
@@ -1,22 +1,23 @@
 import css from './RecipesList.module.css';
 
-export default function RecipesList({ recipes }) {
-  console.log('Recipes: ', recipes);
+export default function RecipesList( ) {
+  /*{console.log('Recipes: ', recipes);
 
   if (!recipes?.length) {
     return (
       <p className={css.message}>There are currently no recipes available.</p>
     );
   }
-
+  }*/
   return (
-    <ul className={css.list}>
+    <div>Recipes list</div>
+    /*{  <ul className={css.list}>
       {recipes.map(recipe => (
         <li key={recipe.id} className={css.item}>
           <h3>{recipe.title}</h3>
           <p>{recipe.description}</p>
         </li>
       ))}
-    </ul>
+    </ul>}*/
   );
 }

1accab8cc00a113b50d879e55700d955d3a4f2c9
Dmytro Molodyk <129516260+Tap0kk@users.noreply.github.com>
2025-06-29 13:19:27 +0300

    Update SearchBox.jsx

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import s from './SearchBox.module.css'
 
 const SearchBox = () => {
-  return <div>SearchBox</div>;
+  return <div className={s.inputContainer}>
+    <input className={s.input} type="text" value="Type here" />
+    <button className={s.inputBtn}>Search</button>
+  </div>;
 };
 
 export default SearchBox;
